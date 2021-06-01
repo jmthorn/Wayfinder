b5c55234f4b7ba0e30b4de76d9becda68e1d65c0
Jessica T <jmthorn16@gmail.com>
2021-06-01 18:30:09 -0500

    add modal context

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,35 +1,65 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import Logo from '../../images/logo.png'
+import { useSelector } from 'react-redux';
+import './navbar.css'
 
 
 const NavBar = () => {
+
+  const user = useSelector(state => state.session.user)
+
+
+  let sessionLinks;
+    if (user) {
+      sessionLinks = (
+        <>
+          <span className="nav-options">
+            <li id="nav-home">
+              <NavLink to="/" exact={true} activeClassName="active">
+                Home
+              </NavLink>
+            </li>
+            <li id="nav-portfolio">
+              <NavLink to="/" exact={true}>
+                Portfolio
+              </NavLink>
+            </li>
+            <li id="nav-logout">
+              <LogoutButton />
+            </li>
+          </span>
+        </>
+      );
+    } else {
+      sessionLinks = (
+        <span className="nav-options">
+          <li id="nav-login">
+            <NavLink to="/login" exact={true} >
+              Log In
+            </NavLink>
+          </li>
+          <li id="nav-signup">
+            <NavLink to="/sign-up" exact={true} >
+              Sign Up
+            </NavLink>
+          </li>
+        </span>
+      );
+    }
+  
+
+
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
+      <ul id="nav-list">
+        <li id="nav-home">
+          <NavLink to="/" exact={true}>
+            <img className="logo" src={Logo} alt="logo" />
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
+        {sessionLinks}
       </ul>
     </nav>
   );
