3560879e2fdf966664efb88f0e7ea52797546fbf
Jessica T <jmthorn16@gmail.com>
2021-06-03 16:49:26 -0500

    added error handling on add trips form

--- react-app/src/components/CreateTripModal/createtripform.js
+++ react-app/src/components/CreateTripModal/createtripform.js
@@ -12,16 +12,24 @@ function CreateTripForm({cityId}) {
 
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
+  const [booked, setBooked] = useState("BOOK")
   const cities = useSelector(state => state.cities.cities)
   const chosenCity = cities.cities.filter((city) => city.id === cityId)[0]
   const chosenCityId = chosenCity.id
   const dispatch = useDispatch();
-  const [errors, setErrors] = useState([]);
+  const [error, setError] = useState("");
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    return dispatch(createTrip(chosenCityId, startDate, endDate))
+    if(endDate < startDate) { 
+      setError("Your end date is before your start date!")
+    } else {
+      setBooked("BOOKED!")
+      return dispatch(createTrip(chosenCityId, startDate, endDate))
+    }
   };  
+
 
   return (
     <>
@@ -30,13 +38,16 @@ function CreateTripForm({cityId}) {
       <div className="modal-form">
           <form onSubmit={(e) => handleSubmit(e)}>
               <label>When are you going to {chosenCity.name}?
+                {error && (
+                  <div className="modal-error">{error}</div>
+                )}
                 <div className="modal-date-inputs">
                     <DatePicker selected={startDate} placeholderText="Start Date" onChange={(date) => setStartDate(date)} />
-                    <div className="modale-to">to</div>
+                    <div className="modal-to">to</div>
                     <DatePicker selected={endDate} placeholderText="End Date" onChange={(date) => setEndDate(date)} />
                 </div>
               </label>
-              <button className="modal-button" type="submit">BOOK</button>
+              <button className="modal-button" type="submit">{booked}</button>
           </form>
       </div>
     </>
