c3283b25b6dacebd6bd60ac138c7d0047c4a3c7b
Jessica T <jmthorn16@gmail.com>
2021-06-03 11:48:01 -0500

    added footer

---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Github from '../../images/25231.png'
+import LinkedIn from '../../images/linkedin-1.png'
+
+import './footer.css'
+
+
+const Footer = () => {
+
+  const handleClick = (e) => { 
+      console.log(e)
+  }
+
+  return (
+    <div id="footer-container">
+        <div className="footer-icons">
+            <a href="https://github.com/jmthorn" className="github_logo">
+                <img src={Github} alt="logo"/>
+            </a>
+            <a href="https://www.linkedin.com/in/jessica-thornton-00615989/" className="github_logo">
+                <img src={LinkedIn} alt="logo"/>
+            </a>
+        </div>
+        <h2>Built By: Jessica Thornton</h2>
+        <div className="footer-description">
+            <p> This itinerary builder was built as a week-long project for a rigorous 1000+ hour software engineering bootcamp. Thank you to Conde Nast Travel for images and content.</p>
+        </div>
+    </div>
+  );
+}
+
+export default Footer;
